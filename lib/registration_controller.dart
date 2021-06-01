db1f697c97b2c9a4d501743a819336b0b9620235
Paiman <paiman.bandi@gmail.com>
2021-06-01 08:48:57 +0700

    textformfield dob

--- lib/registration_controller.dart
+++ lib/registration_controller.dart
@@ -1,28 +1,54 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:rgstr/registration_page.dart';
 import 'package:rgstr/registration_view.dart';
 
 import 'package:imei_imsi_plugin/imei_imsi_plugin.dart';
+import 'package:device_info/device_info.dart';
 
 class RegistrationController extends State<RegistrationPage> {
-  TextEditingController tecImei = TextEditingController();
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  TextEditingController date = new TextEditingController();
+  DateTime selectedDate = DateTime.now();
+
+  Future<Null> selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(context: context, initialDate: selectedDate, firstDate: DateTime(1901, 1), lastDate: DateTime(2100));
+    if (picked != null && picked != selectedDate) {
+      String formattedDateTime = "${picked.day.toString().padLeft(2, '0')}/${picked.month.toString().padLeft(2, '0')}/${picked.year.toString()}";
+
+      setState(() {
+        selectedDate = picked;
+        date.value = TextEditingValue(text: formattedDateTime);
+      });
+    }
+  }
 
   String sPlatformImei = '';
 
   @override
   void initState() {
+    Future.delayed(Duration.zero, () async {
+      initPlatformState();
+    });
     super.initState();
-    initPlatformState();
   }
 
   Future<void> initPlatformState() async {
     String platformImei;
+
     try {
+      var androidInfo = await deviceInfoPlugin.androidInfo;
+      var sdkInt = androidInfo.version.sdkInt;
       // only works in Android version < 10
-      platformImei = await ImeiImsiPlugin.getImei(shouldShowRequestPermissionRationale: false);
+      if ((Platform.isAndroid && sdkInt < 29) || Platform.isIOS) {
+        platformImei = await ImeiImsiPlugin.getImei(shouldShowRequestPermissionRationale: false);
+      } else {
+        platformImei = '';
+      }
     } on PlatformException {
-      print('Failed to get platform version.');
+      print('Failed to get IMEI.');
     }
 
     if (!mounted) return;
--- lib/registration_view.dart
+++ lib/registration_view.dart
@@ -16,91 +16,125 @@ class RegistrationView extends WidgetView<RegistrationPage, RegistrationControll
     bool _bErrImei = false;
     bool _bErrFirstName = false;
     bool _bErrLastName = false;
+    bool _bErrDob = false;
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
           centerTitle: true,
         ),
         body: Padding(
-          padding: EdgeInsets.all(30),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Text(
-                        'IMEI',
-                        style: textStyle,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: TextFormField(
-                        inputFormatters: [
-                          LengthLimitingTextInputFormatter(17),
+            padding: EdgeInsets.all(30),
+            child: Center(
+              child: Column(mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.center, children: <Widget>[
+                SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              'IMEI',
+                              style: textStyle,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: TextFormField(
+                              inputFormatters: [
+                                LengthLimitingTextInputFormatter(17),
+                              ],
+                              initialValue: state.sPlatformImei,
+                              decoration: InputDecoration(
+                                hintText: 'IMEI on your phone',
+                                errorText: _bErrImei ? "Can't be empty" : null,
+                              ),
+                              style: textStyle,
+                            ),
+                          )
                         ],
-                        initialValue: state.sPlatformImei,
-                        decoration: InputDecoration(
-                          errorText: _bErrImei ? "Can't be empty" : null,
-                        ),
-                        style: textStyle,
                       ),
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Text(
-                        'First Name',
-                        style: textStyle,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: TextFormField(
-                        inputFormatters: [
-                          LengthLimitingTextInputFormatter(50),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              'First Name',
+                              style: textStyle,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: TextFormField(
+                              inputFormatters: [
+                                LengthLimitingTextInputFormatter(50),
+                              ],
+                              decoration: InputDecoration(
+                                hintText: 'Your first name',
+                                errorText: _bErrFirstName ? "Can't be empty" : null,
+                              ),
+                              style: textStyle,
+                            ),
+                          )
                         ],
-                        decoration: InputDecoration(
-                          errorText: _bErrFirstName ? "Can't be empty" : null,
-                        ),
-                        style: textStyle,
                       ),
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Text(
-                        'Last Name',
-                        style: textStyle,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: TextFormField(
-                        inputFormatters: [
-                          LengthLimitingTextInputFormatter(50),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              'Last Name',
+                              style: textStyle,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: TextFormField(
+                              inputFormatters: [
+                                LengthLimitingTextInputFormatter(50),
+                              ],
+                              decoration: InputDecoration(
+                                hintText: 'Your last name',
+                                errorText: _bErrLastName ? "Can't be empty" : null,
+                              ),
+                              style: textStyle,
+                            ),
+                          )
                         ],
-                        decoration: InputDecoration(
-                          errorText: _bErrLastName ? "Can't be empty" : null,
-                        ),
-                        style: textStyle,
                       ),
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ));
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Text(
+                              'DoB',
+                              style: textStyle,
+                            ),
+                          ),
+                          Expanded(
+                              flex: 3,
+                              child: GestureDetector(
+                                onTap: () => state.selectDate(context),
+                                child: AbsorbPointer(
+                                  child: TextFormField(
+                                    controller: state.date,
+                                    keyboardType: TextInputType.datetime,
+                                    decoration: InputDecoration(
+                                      hintText: 'Date of Birth',
+                                      errorText: _bErrDob ? "Can't be empty" : null,
+                                      prefixIcon: Icon(
+                                        Icons.calendar_today,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ))
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ]),
+            )));
   }
 }
